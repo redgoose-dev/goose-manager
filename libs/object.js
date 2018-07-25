3f5ac456c1f5234b6048c4cadf959ce3bb290bd8
redgoose <scripter@me.com>
2018-07-25 19:16:14 +0900

    nests 목록 작업중.. (삽질 거하게 하고있음)

---
+++ libs/object.js
@@ -0,0 +1,40 @@
+/**
+ * array to object
+ * 배열에서 객체로 변환시켜주는 툴
+ *
+ * @param {Array} arr
+ * @param {String} key
+ * @param {Function} hook
+ * @return {Object}
+ */
+export function arrayToObject(arr=[], key=null, hook=null)
+{
+	let result = {};
+	arr.forEach((o, k) => {
+		if (key)
+		{
+			let n = o[key] || 0;
+			if (hook) o = hook(o);
+			if (!(result[n] && result[n].children && Array.isArray(result[n].children)))
+			{
+				result[n] = { children: [] };
+			}
+			result[n].children.push(o);
+		}
+		else
+		{
+			if (hook) o = hook(o);
+			result[k] = o;
+		}
+	});
+	return result;
+}
+
+export function getObjectValueToArray(arr, key)
+{
+	let result = [];
+	arr.forEach((o) => {
+		if (o[key]) result.push(o[key]);
+	})
+	return result;
+}
--- pages/nests/index.vue
+++ pages/nests/index.vue
@@ -11,6 +11,9 @@
 <script>
 import PageHeader from '~/components/contents/page-header';
 import ButtonBasic from '~/components/button/basic';
+import * as messages from '../../libs/messages';
+import * as dates from '../../libs/dates';
+import * as object from '../../libs/object';
 
 export default {
 	components: {
@@ -21,20 +24,72 @@ export default {
 	{
 		try
 		{
-			let res = await cox.$axios.$get('/nests?order=srl&sort=desc');
-			console.log(res);
+			// get apps
+			let apps = await cox.$axios.$get('/apps');
+			if (!apps.success) throw 'Failed get apps';
+			apps = apps.data.index;
+			// get nests
+			let seqs = apps.map((o) => cox.$axios.$get(`/nests?app=${o.srl}&unlimit=1`));
+			seqs.push(cox.$axios.$get(`/nests?app=NULL&unlimit=1`));
+			let nests = await Promise.all(seqs);
+			// merge apps and nests
+			let tree = [];
+			apps.forEach((o, k) => {
+				let res = {
+					srl: parseInt(o.srl),
+					id: o.id,
+					name: o.name,
+				};
+				if (nests[k].data)
+				{
+					res.nests = nests[k].data.index;
+				}
+				else
+				{
+					res.nests = [];
+				}
+				tree.push(res);
+			});
+			// get no app data
+			console.log(nests[nests.length-1].data)
+			if (nests[nests.length-1].data);
+			{
+				tree.push({
+					srl: null,
+					id: null,
+					name: 'no app',
+					nests: nests[nests.length-1].data.index
+				});
+			}
+			console.log('========');
+			console.log(tree);
+
+			// TODO: 대충 데이터를 만들었는데 요청이 너무많이 일어남.
+			// TODO: 그래서 커스텀화한 전용 API를 만들기로 결정했음.
+			// TODO: 주소는 `/custom/manager-nests`으로..
+			// TODO: api 먼저 제작해야함.
+
+			// let keys = object.getObjectValueToArray(apps, 'srl');
+			//console.log(object.arrayToObject(apps));
+			// console.log(keys);
+
+			// let res = await cox.$axios.$get('/nests?order=srl&sort=desc');
 			// if (!(res && res.success)) throw messages.service.noItem;
-			// return {
-			// 	index: res.data.index.map((o) => {
-			// 		o.srl = parseInt(o.srl);
-			// 		o.regdate = dates.getFormatDate(o.regdate, false);
-			// 		o.level = parseInt(o.level);
-			// 		return o;
-			// 	})
-			// };
+			// let nests = Object.assign([], res.data.index);
+			// let tree = object.arrayToObject(nests, 'app_srl', (o) => {
+			// 	o.regdate = dates.getFormatDate(o.regdate, false);
+			// 	return o;
+			// });
+
+			//console.log(Object.keys(tree));
+
+			return {
+				index: []
+			};
 		}
 		catch(e)
 		{
+			console.error(e)
 			return { error: (typeof e === 'string') ? e : messages.error.service };
 		}
 	}
