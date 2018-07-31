974a3cfc1fd6dffa525d4861e4b08414bbe9c885
redgoose <scripter@me.com>
2018-07-31 18:45:20 +0900

    nest 폼 일부작업

--- components/pages/nests/post.vue
+++ components/pages/nests/post.vue
@@ -17,18 +17,52 @@
 							{ label: 'Default', value: 'default' },
 							{ label: 'Advanced', value: 'advanced' },
 						]"/>
+					<p class="rg-form-help">
+						skin을 변경하면 `extra form`의 항목이 변하는것을 주의하세요.
+					</p>
 				</dd>
 			</dl>
 			<dl class="rg-form-field">
-				<dt><label for="nest_skin">Nest skin</label></dt>
+				<dt><label for="nest_skin">App</label></dt>
 				<dd>
-					sdmgidgopgds
+					select form
 				</dd>
 			</dl>
 			<dl class="rg-form-field">
-				<dt><label for="nest_skin">Nest skin</label></dt>
+				<dt><label for="nest_skin">Article skin</label></dt>
 				<dd>
-					sdmgidgopgds
+					list of article skins<br/>
+					TODO: 미리 변수화된 스킨 목록을 만들어두고 선택하게 하기
+				</dd>
+			</dl>
+			<dl class="rg-form-field">
+				<dt><label for="nest_skin">ID</label></dt>
+				<dd>
+					nest id
+				</dd>
+			</dl>
+			<dl class="rg-form-field">
+				<dt><label for="nest_skin">Name</label></dt>
+				<dd>
+					nest name
+				</dd>
+			</dl>
+			<dl class="rg-form-field">
+				<dt><label for="nest_skin">Size</label></dt>
+				<dd>
+					페이지당 글 갯수
+				</dd>
+			</dl>
+			<dl class="rg-form-field">
+				<dt><label for="nest_skin">Use category</label></dt>
+				<dd>
+					분류사용
+				</dd>
+			</dl>
+			<dl class="rg-form-field">
+				<dt><label for="nest_skin">권한설정</label></dt>
+				<dd>
+					읽기권한, 쓰기권한
 				</dd>
 			</dl>
 		</div>
@@ -56,6 +90,7 @@ import FormChecks from '~/components/form/checks';
 // library
 import { checkId, formData } from '~/libs/forms';
 import * as messages from '~/libs/messages';
+import * as articlePreference from '~/libs/articles/preference';
 
 export default {
 	components: {
@@ -85,6 +120,10 @@ export default {
 			error: null,
 			processing: false,
 		};
+	},
+	mounted()
+	{
+		console.log(articlePreference.skin);
 	},
 	methods: {
 		async onSubmit(e)
---
+++ libs/articles/preference.js
@@ -0,0 +1,2 @@
+// article skin
+export const skin = [1,2,3];
