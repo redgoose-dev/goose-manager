23f705488eb4b55183de959253f754a29f3b1cda
redgoose <scripter@me.com>
2020-07-18 15:13:16 +0900

    #37 청크파일이름 앞에 글자를 붙인 임시처방

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ const sessionStore = (DB_HOST && DB_PORT && DB_USERNAME && DB_PASSWORD && DB_DAT
 
 // nuxt config
 module.exports = {
+  mode: 'universal',
   head: {
     title: APP_NAME,
     meta: [
@@ -36,6 +37,17 @@ module.exports = {
   ],
   build: {
     extractCSS: process.env.NODE_ENV === 'production',
+    indicator: false,
+    filenames: {
+      /**
+       * TODO:
+       * 2.13.x 버전에서 `/.nuxt/dist/client/commons/.efldf.js` 형태의 숨겨진 js 파일로 만들어진다.
+       * https://github.com/nuxt/nuxt.js/issues/7703 이슈에서 잠시 대처할 수 있는 방법이 적혀있어 대처했다.
+       * 2.14.x 버전에서 패치될 예정인걸로 보이므로(https://github.com/nuxt/nuxt.js/pull/7667) 버전이 오르면 이 코드는 삭제하고 다시 테스트 해봐야할 것이다.
+       * 다음 코드는 자연스럽게 해결하는 방법이 아니기 때문에 나중에는 빼야할 것이다.
+       */
+      chunk: ({ isDev }) => isDev ? 'c[name].js' : 'c[id].[contenthash].js'
+    },
   },
   modules: [
     '@nuxtjs/axios',
@@ -82,5 +94,4 @@ module.exports = {
     }),
     '~/api',
   ],
-  telemetry: false,
 };
