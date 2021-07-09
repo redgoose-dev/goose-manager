50671c6e9e486f0635341cf1266392dc04229fe8
redgoose <scripter@me.com>
2021-07-09 17:55:59 +0900

    포커스 빼기로 했다. ㅠ

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,8 +12,7 @@
               :is="getNavigationElementName(item)"
               :to="item.link"
               :href="item.href"
-              :target="item.target"
-              tabindex="0">
+              :target="item.target">
               <em>{{item.name}}</em>
               <icon v-if="item.icon" :name="item.icon"/>
               <icon v-if="item.children && item.children.length" name="chevron-down" class="flip-y dropdown"/>
@@ -25,8 +24,7 @@
                     :is="getNavigationElementName(item2)"
                     :to="item2.link"
                     :href="item2.href"
-                    :target="item2.target"
-                    tabindex="0">
+                    :target="item2.target">
                     <em>{{item2.name}}</em>
                     <icon v-if="item2.icon" :name="item2.icon"/>
                     <icon v-if="item2.children && item2.children.length" name="chevron-right" class="dropdown"/>
@@ -38,8 +36,7 @@
                           :is="getNavigationElementName(item3)"
                           :to="item3.link"
                           :href="item3.href"
-                          :target="item3.target"
-                          tabindex="0">
+                          :target="item3.target">
                           <em>{{item3.name}}</em>
                           <icon v-if="item3.icon" :name="item3.icon"/>
                         </component>
@@ -55,7 +52,7 @@
       <nav v-if="!!user" class="header-nav">
         <ul class="navigation navigation--side">
           <li>
-            <span tabindex="0">
+            <span>
               <em>{{user.email}}</em>
               <icon name="chevron-down" class="flip-y"/>
             </span>
