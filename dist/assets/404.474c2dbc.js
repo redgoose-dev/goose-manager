0851a1c79e7ff4744b2b7001aa59f2cb265dc0f5
redgoose <scripter@me.com>
2022-03-18 10:53:18 +0900

    dist 삭제

--- dist/assets/404.474c2dbc.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.24d3ff97.js";import{o,e as r}from"./vendor.cc7e59d8.js";const n={};function c(t,a){return o(),r("p",null,"not found page")}var _=e(n,[["render",c]]);export{_ as default};
--- dist/assets/index.d540a613.js
+++
@@ -1,1 +0,0 @@
-import{o as a,e as t,f as s,v as o,u as _,z as d,A as r}from"./vendor.cc7e59d8.js";import{_ as c,s as p}from"./index.24d3ff97.js";const n=e=>(d("data-v-dfd0afe0"),e=e(),r(),e),f=n(()=>s("h1",null,"Dashboard",-1)),l={setup(e){return(i,u)=>(a(),t("article",null,[f,s("pre",null,o(_(p).state),1)]))}};var h=c(l,[["__scopeId","data-v-dfd0afe0"]]);export{h as default};
