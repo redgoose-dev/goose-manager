48f446be8b1d175eda7e6c81af1618de932cde13
redgoose <scripter@me.com>
2022-07-19 12:51:11 +0900

    사용안하는 디펜던시 삭제

--- server/entry.js
+++ server/entry.js
@@ -1,25 +1,25 @@
-import fs from 'fs';
-import path from 'path';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import { createServer, loadEnv } from 'vite';
-import { openServerMessage, isDev } from './libs/entry-assets.js';
-import serviceServer from './index.js';
+import fs from 'fs'
+import path from 'path'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import { createServer, loadEnv } from 'vite'
+import { openServerMessage, isDev } from './libs/entry-assets.js'
+import serviceServer from './index.js'
 
-const __dirname = path.resolve();
+const __dirname = path.resolve()
 
 async function server()
 {
-  const app = express();
-  const dev = isDev();
-  const env = loadEnv(dev ? 'development' : 'production', __dirname);
-  const outDir = env.VITE_OUT_DIR || 'dist';
-  const clientPath = dev ? __dirname : path.resolve(outDir);
+  const app = express()
+  const dev = isDev()
+  const env = loadEnv(dev ? 'development' : 'production', __dirname)
+  const outDir = env.VITE_OUT_DIR || 'dist'
+  const clientPath = dev ? __dirname : path.resolve(outDir)
 
   // setup
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
-  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: false }))
+  app.use(express.json())
+  app.use(cookieParser())
 
   if (dev)
   {
@@ -31,56 +31,56 @@ async function server()
         middlewareMode: true,
       },
       appType: 'custom',
-    });
-    app.use(vite.middlewares);
+    })
+    app.use(vite.middlewares)
     // extend routes
-    serviceServer(app, env);
+    serviceServer(app, env)
     // global route
     app.use('*', async (req, res) => {
       try
       {
-        const url = req.originalUrl;
-        let template = fs.readFileSync(path.resolve(clientPath, 'index.html'), 'utf-8');
-        template = await vite.transformIndexHtml(url, template);
-        res.status(200).set({ 'Content-Type': 'text/html' }).end(template);
+        const url = req.originalUrl
+        let template = fs.readFileSync(path.resolve(clientPath, 'index.html'), 'utf-8')
+        template = await vite.transformIndexHtml(url, template)
+        res.status(200).set({ 'Content-Type': 'text/html' }).end(template)
       }
       catch (e)
       {
-        vite.ssrFixStacktrace(e);
-        res.status(500).end(e.message);
+        vite.ssrFixStacktrace(e)
+        res.status(500).end(e.message)
       }
-    });
+    })
   }
   else
   {
     // # Production
     // extend routes
-    serviceServer(app, env);
+    serviceServer(app, env)
     // global route
     app.use((req, res, next) => {
       if (req.path.indexOf('.') > -1)
       {
         // https://expressjs.com/en/5x/api.html#express.static
-        app.use('/', express.static(outDir));
-        app.use(env.VITE_BASE_URL, express.static(outDir));
-        next();
+        app.use('/', express.static(outDir))
+        app.use(env.VITE_BASE_URL, express.static(outDir))
+        next()
       }
       else
       {
-        let template = fs.readFileSync(path.resolve(clientPath, 'index.html'), 'utf-8');
+        let template = fs.readFileSync(path.resolve(clientPath, 'index.html'), 'utf-8')
         res
           .status(200)
           .set({ 'Content-Type': 'text/html' })
-          .end(template);
+          .end(template)
       }
-    });
+    })
   }
 
   // listen server
   app.listen(Number(env.VITE_PORT) || 3000, env.VITE_HOST, () => {
-    openServerMessage(env.VITE_HOST, Number(env.VITE_PORT) || 3000, dev);
-  });
+    openServerMessage(env.VITE_HOST, Number(env.VITE_PORT) || 3000, dev)
+  })
 }
 
 // actions
-server().then();
+server().then()
--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import { Router } from 'express';
+import { $fetch } from 'ohmyfetch'
+import { Router } from 'express'
 
-let cookie, app, env, api;
+let cookie, app, env, api
 
 /**
  * local routes
  */
 function localRoutes()
 {
-  const router = Router();
+  const router = Router()
 
   // auth
   router.post('/auth/', async (req, res) => {
     try
     {
-      let token = req.cookies[cookie.prefix + '-token'];
-      let user = req.cookies[cookie.prefix + '-user'];
-      user = JSON.parse(user);
-      if (!user.srl) throw new Error('Invalid user data');
+      let token = req.cookies[cookie.prefix + '-token']
+      let user = req.cookies[cookie.prefix + '-user']
+      user = JSON.parse(user)
+      if (!user.srl) throw new Error('Invalid user data')
       res.json({
         success: true,
         data: { token, user },
-      });
+      })
     }
     catch(e)
     {
@@ -30,66 +30,66 @@ function localRoutes()
         message: 'Failed authorization.',
       })
     }
-  });
+  })
 
   // login
   router.post('/login/', async (req, res) => {
     try
     {
-      const { email, password, save } = req.body;
+      const { email, password, save } = req.body
       // request to api
-      const apiRes = await api.post('/auth/login/', {
-        email,
-        password,
-      });
+      const { success, message, data } = await api('/auth/login/', {
+        method: 'post',
+        body: {
+          email,
+          password,
+        },
+      })
       // check response values
-      if (!(apiRes.data.success && !!apiRes.data?.data))
-      {
-        throw new Error(apiRes.data.message);
-      }
-      let token = apiRes.data.data.token;
-      let user = apiRes.data.data;
-      delete user.token;
-      delete user.host;
+      if (!(success && !!data)) throw new Error(message)
+      let user = data
+      let token = user.token
+      delete user.token
+      delete user.host
       // set cookie
       if (save)
       {
-        res.cookie(cookie.prefix + '-token', token, cookie.options);
-        res.cookie(cookie.prefix + '-user', JSON.stringify(user, null, ''), cookie.options);
+        res.cookie(cookie.prefix + '-token', token, cookie.options)
+        res.cookie(cookie.prefix + '-user', JSON.stringify(user, null, ''), cookie.options)
       }
       // result
       res.json({
         success: true,
         data: { user, token },
-      });
+      })
     }
     catch (e)
     {
       res.json({
         success: false,
         message: e.message || 'Failed login',
-      });
+      })
     }
-  });
+  })
 
   // logout
   router.post('/logout/', async (req, res) => {
-    res.clearCookie(cookie.prefix + '-token');
-    res.clearCookie(cookie.prefix + '-user');
-    res.json({ success: true });
-  });
+    res.clearCookie(cookie.prefix + '-token')
+    res.clearCookie(cookie.prefix + '-user')
+    res.json({ success: true })
+  })
 
   // refresh token
   router.post('/refresh-token/', async (req, res) => {
-    const { token } = req.body;
-    if (!token) return;
-    res.cookie(cookie.prefix + '-token', token, cookie.options);
-    res.json({ token });
-  });
+    const { token } = req.body
+    if (!token) return
+    res.cookie(cookie.prefix + '-token', token, cookie.options)
+    res.json({ token })
+  })
 
   // TODO: 로그를 기록하는 기능 만들기
 
-  return router;
+  return router
 }
 
 /**
@@ -100,9 +100,9 @@ function localRoutes()
  */
 function setup(_app, _env)
 {
-  app = _app;
-  env = _env;
-  const { VITE_COOKIE_PREFIX, VITE_COOKIE_EXPIRY, VITE_TOKEN_PUBLIC, VITE_API_URL } = env;
+  app = _app
+  env = _env
+  const { VITE_COOKIE_PREFIX, VITE_COOKIE_EXPIRY, VITE_TOKEN_PUBLIC, VITE_API_URL } = env
   // set cookie assets
   cookie = {
     prefix: VITE_COOKIE_PREFIX,
@@ -112,12 +112,14 @@ function setup(_app, _env)
       secure: true,
       path: '/',
     }
-  };
+  }
   // set api instance
-  api = new axios.create({
+  api = $fetch.create({
     baseURL: VITE_API_URL,
-    headers: { 'Authorization': 'Bearer ' + VITE_TOKEN_PUBLIC },
-  });
+    retry: 0,
+    responseType: 'json',
+    headers: { 'Authorization': `Bearer ${VITE_TOKEN_PUBLIC}` },
+  })
 }
 
 /**
@@ -128,11 +130,11 @@ function setup(_app, _env)
  */
 function server(_app, _env)
 {
-  setup(_app, _env);
+  setup(_app, _env)
   // set local routes
-  let path = `${env.VITE_BASE_URL}/local/`;
-  path = path.replace(/\/\//gi, '/');
-  app.use(path, localRoutes());
+  let path = `${env.VITE_BASE_URL}/local/`
+  path = path.replace(/\/\//gi, '/')
+  app.use(path, localRoutes())
 }
 
-export default server;
+export default server
