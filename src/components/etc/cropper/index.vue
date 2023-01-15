ef404ed593e675a3d5d3cf2b3c8447f99a90dbec
redgoose.eth <scripter@me.com>
2023-01-15 17:06:49 +0900

    이미지 리사이즈 모듈 업데이트 및 썸네일 이미지 만들기 샤픈적용

--- src/components/etc/cropper/index.vue
+++ src/components/etc/cropper/index.vue
@@ -3,53 +3,54 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, watch } from 'vue';
-import Croppie from 'croppie';
-import ImageResize from 'image-resize';
-import { pureObject } from '../../../libs/object';
-import 'croppie/croppie.css';
+import { ref, onMounted, onUnmounted, watch } from 'vue'
+import Croppie from 'croppie'
+import ImageResize from 'image-resize'
+import { pureObject } from '../../../libs/object'
+import 'croppie/croppie.css'
 
-const $root = ref();
+const $root = ref()
 const props = defineProps({
   image: String,
   boundary: Object,
   viewport: Object,
   modelValue: Object,
-});
-const emits = defineEmits([ 'update:modelValue' ]);
-let croppie;
+})
+const emits = defineEmits([ 'update:modelValue' ])
+let croppie
 
 function onSubmit(data)
 {
-  emits('update:modelValue', data);
+  emits('update:modelValue', data)
 }
 
 async function output()
 {
-  const width = props.viewport.width || 320;
-  const height = props.viewport.height || 240;
+  const width = props.viewport.width || 320
+  const height = props.viewport.height || 240
   let output = await croppie.result({
     type: 'base64',
     format: 'png',
     size: { width: width * 2, height: height * 2 },
-  });
+  })
   let resize = new ImageResize({
     format: 'webp',
     outputType: 'base64',
-    quality: .82,
+    quality: .9,
     width,
     height,
     reSample: 4,
-  });
-  output = await resize.play(output);
-  return output;
+    sharpen: .25,
+  })
+  output = await resize.play(output)
+  return output
 }
 
 watch(() => props.image, image => croppie.bind({
   url: image,
   points: props.modelValue.points,
   zoom: props.modelValue.zoom,
-}));
+}))
 
 onMounted(() => {
   try
@@ -57,32 +58,32 @@ onMounted(() => {
     croppie = new Croppie($root.value, pureObject({
       boundary: props.boundary || { width: '100%', height: '100%' },
       viewport: props.viewport || { width: 320, height: 240, type: 'square' },
-    }));
+    }))
     croppie.bind(pureObject({
       url: props.image,
       points: props.modelValue.points || undefined,
       zoom: props.modelValue.zoom || undefined,
       orientation: props.modelValue.orientation || undefined,
-    }));
+    }))
     // croppie
     setTimeout(() => {
-      $root.value.addEventListener('update', e => onSubmit(e.detail));
-    }, 200);
+      $root.value.addEventListener('update', e => onSubmit(e.detail))
+    }, 200)
   }
   catch (e)
   {
     console.error(e)
   }
-});
+})
 onUnmounted(() => {
-  if (!croppie) return;
-  croppie.destroy();
-  croppie = undefined;
-});
+  if (!croppie) return
+  croppie.destroy()
+  croppie = undefined
+})
 
 defineExpose({
   output,
-});
+})
 </script>
 
 <style src="./index.scss" lang="scss" scoped></style>
